38e222de4397eda7a66d278a6ce015105e5a5d71
0MrDarn0 <Darn@live.de>
2020-12-12 06:21:50 +0100

    -

--- premake5.lua
+++ premake5.lua
@@ -5,33 +5,8 @@ project "yaml-cpp"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"src/**.h",
-		"src/**.cpp",
-		"include/**.h"
-	}
+	files { "src/**.h", "src/**.cpp", "include/**.h" }
+	includedirs { "include" }
 
-	includedirs
-	{
-		"include"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+	MSVC_Options()
+	Configuration_Options()
